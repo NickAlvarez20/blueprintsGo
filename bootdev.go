c850556d4b75b6476af77e7b1d10bb6e407598fd
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-01-31 14:00:28 -0700

    Created function test if car can send message. | Check if radius and material fields contains values, returns false. | If contains values, returns true.

--- bootdev.go
+++ bootdev.go
@@ -20,10 +20,24 @@ type car struct{
 	backWheel wheel //of type wheel
 }
 
+
 //Creating Nested Struct
 type wheel struct{
 	radius int
 	material string
+}
+
+//Creating a function to test nested structures
+func canSendMessage(mToSend car) bool{
+	//Check if sender has a non-empty name and a non zero number
+	if mToSend.backWheel.radius == 0 || mToSend.backWheel.material == ""{
+		return false
+	}
+	if mToSend.frontWheel.radius == 0 || mToSend.frontWheel.material == ""{
+		return false
+	}
+	//Return true is the fields are populated with values 
+	return true
 }
 
 //Accessing field using dot notation
@@ -31,6 +45,7 @@ type wheel struct{
 func main(){
 	//Initialize a new car
 	UniverseCar := car{brand: "Tesla", model: "Roaster", doors: 4, mileage: 100000, frontWheel: wheel{radius: 100, material: "Plywood"}, backWheel: wheel{radius: 200, material: "Glue"}}
+	NewCar := car{}
 	//Create a value for wheel
 	backWheelRadius := UniverseCar.backWheel.radius
 	frontWheelRadius := UniverseCar.frontWheel.radius
@@ -38,6 +53,9 @@ func main(){
 
 	fmt.Printf("The radius of the front wheel is %d, and the back wheel radius is %d\n", backWheelRadius, frontWheelRadius)
 	fmt.Printf("The material made in front wheel of the %s is %s. The material used in making the back wheel of the %s is %s.\n", UniverseCar.brand, UniverseCar.frontWheel.material, UniverseCar.brand, UniverseCar.backWheel.material)
+
+	fmt.Println(canSendMessage(UniverseCar))
+	fmt.Println(canSendMessage(NewCar))
 }
 
 // go run bootdev.go
